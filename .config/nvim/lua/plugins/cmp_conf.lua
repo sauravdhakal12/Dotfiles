dd1e79473645dc9c5d8242c8a69308a2e3dd054b
sauravdhakal12 <sauravdhakal121@gmail.com>
2022-02-12 21:01:33 +0545

    cmp_conf file updated based on official page

--- .config/nvim/lua/plugins/cmp_conf.lua
+++ .config/nvim/lua/plugins/cmp_conf.lua
@@ -2,6 +2,17 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
+local lspconfig = require('lspconfig')
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = {'pyright', 'sumneko_lua'}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
 -- luasnip setup
 local luasnip = require 'luasnip'
 
@@ -10,7 +21,7 @@ local cmp = require 'cmp'
 cmp.setup {
   snippet = {
     expand = function(args)
-      luaship.lsp_expand(args.body)
+      require('luasnip').lsp_expand(args.body)
     end,
   },
   mapping = {
